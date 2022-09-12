a4ac96810f2ff4340e6f4838cd13262a592bed52
ezioweb <ezioweb@hotmail.com>
2022-09-12 16:45:25 -0300

    Notification String

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -38,7 +38,7 @@ public class SmsService {
         PhoneNumber to = new PhoneNumber(twilioPhoneTo);
         PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-        Message message = Message.creator(to, from, "teste").create();
+        Message message = Message.creator(to, from, msg).create();
 
         System.out.println(message.getSid());
     }
